023513d9faf4fc05330509ca2ef7960e154eb5a0
Miles Mao <mrmao1@gmail.com>
2023-02-06 12:39:59 -0600

    JS Variables exercises results

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 & 2
+const firstName = 'Miles';
+let lastName = 'Mao';
+var age = 46;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 465;
+let hasRead = true;
